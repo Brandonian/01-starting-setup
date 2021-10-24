69112391ab934001b4be6ff9d452d8deb751dee3
Brandon Webster <bwwebs@gmail.com>
2021-10-23 19:58:34 -0700

    Testing from vs code

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,7 +4,7 @@ function ExpenseItem() {
             <div>September 12th, 2021</div>
             <div>
                 <h2>Car Insurance</h2>
-                <div>$692.00</div>
+                <div>$693.00</div>
             </div>
         </div>
     );
