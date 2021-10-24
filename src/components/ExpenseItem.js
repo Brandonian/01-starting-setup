2b836165a2c7ce985e06096f53cdd2dc9fc7d737
Brandon Webster <bwwebs@gmail.com>
2021-10-23 19:49:57 -0700

    Initial commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div>September 12th, 2021</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>$693.00</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
