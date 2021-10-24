0377020969057c108f2d72c482e77165a02ce9db
Brandon Webster <bwwebs@gmail.com>
2021-10-23 20:16:40 -0700

    Apply css styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
     return (
-        <div>
+        <div className="expense-item">
             <div>September 12th, 2021</div>
-            <div>
+            <div className="expense-item__description">
                 <h2>Car Insurance</h2>
-                <div>$693.00</div>
+                <div className="expense-item__price">$693.00</div>
             </div>
         </div>
     );
